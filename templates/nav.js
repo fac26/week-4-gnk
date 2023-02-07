b470bd43ba9f698f9f207267532b23f3d7d15f4a
Georgia E. Whitney <81175151+rockyrelay@users.noreply.github.com>
2023-02-07 11:14:51 +0000

    added nav html to templates

---
+++ templates/nav.js
@@ -0,0 +1,16 @@
+function navbar() {
+  return /*html*/ `
+  <nav>
+        <ul class="main-nav">
+            <li>Home</li>
+        </ul>
+        <ul class="auth-nav">
+            <li>Log in</li>
+            <li>Sign up</li>
+        </ul>
+    </nav>
+  `
+
+}
+
+module.exports = { navbar };
