e0605c2e8b43158378ae67fe6f6a6bca2a4c9c37
Georgia E. Whitney <81175151+rockyrelay@users.noreply.github.com>
2023-02-08 16:05:11 +0000

    fixed issue with semi colons and removed .env

--- src/api.js
+++ src/api.js
@@ -1,31 +1,33 @@
+const dotenv = require('dotenv');
+dotenv.config();
 // Node doesn't have fetch built-in
 // so we have to npm install one
-const fetch = require("node-fetch");
+const fetch = require('node-fetch');
 
 const client_id = process.env.CLIENT_ID;
 const client_secret = process.env.CLIENT_SECRET;
 
-const TOKEN_URL = "https://github.com/login/oauth/access_token";
+const TOKEN_URL = 'https://github.com/login/oauth/access_token';
 
 function getToken(code) {
   const body = { client_id, client_secret, code };
-  console.log(JSON.stringify(body));
+  console.log(JSON.stringify(body))
   return fetch(TOKEN_URL, {
-    method: "POST",
+    method: 'POST',
     body: JSON.stringify(body),
     // IMPORTANT: THESE HEADERS ARE REQUIRED
     // GH will do weird 404 errors if you don't specify exactly what data type you're sending
-    headers: { accept: "application/json", "content-type": "application/json" }
+    headers: { accept: 'application/json', 'content-type': 'application/json' },
   })
     .then(getJson)
-    .then(data => data.access_token);
+    .then((data) => data.access_token);
 }
 
-const USER_URL = "https://api.github.com/user";
+const USER_URL = 'https://api.github.com/user';
 
 function getUser(token) {
   return fetch(USER_URL, {
-    headers: { accept: "application/json", authorization: `token ${token}` }
+    headers: { accept: 'application/json', authorization: `token ${token}` },
   }).then(getJson);
 }
 
@@ -33,7 +35,7 @@ function getJson(response) {
   if (!response.ok) {
     console.log(response);
     const error = new Error('HTTP Error');
-    error.status = response.status;
+    error.status = response.statusCode;
     throw error;
   }
   return response.json();
